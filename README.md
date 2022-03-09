691e5f7f643d4812ca9a67d9e40253738e211f29
atfedi <afedi.ieu2020@student.ie.edu>
2022-03-09 16:52:40 +0100

    First Assignment

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Please describe the rules in this list:
 2. The hero and the dragon have a set of "hit points"
 3. The hit points value is read from the user
 4. The hero and the dragon have a set life points (hp)
-5. The hp for the dragon are given, while the one for the hero are choosen by the user, or if not choosen are already given by the game
-6. Using a while loop, the game is repeated until one of the two condentenders die.
+5. All the data can be choosen by the player, if are not choosen, a set of given value are going to be used in the game
+6. Using a while loop, the dragon starts the fight, than the hero will attack, the game is repeated until one of the two condentenders die.
 
--- game.py
+++ game.py
@@ -1,42 +1,40 @@
 import random
-# import random is a simple library that implements the random number generator behavior
 
-# Please read into the variables below the correct numbers. Use try and except to catch error.
-# a simple example would be:
 try:
     hero_hp = int(input("how many hp does the hero have? "))
 except:
     hero_hp = 50
+try:
+    dragon_hp = int(input("how many hp does the dragon have? "))
+except:
     dragon_hp = 100
+try:
+    hero_max_dmg = int(input("what is the max damage caused by the hero? "))
+except:
     hero_max_dmg = 20
+try:
+    dragon_max_dmg = int(input("what is the max damage caused by the dragon? "))
+except:
     dragon_max_dmg = 10
-
-print("The dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp")
-
-# add a While for an infinite block
-while dragon_hp > 0:
+finally:
+    print("\nThe dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp\n")
+while True:
     dragon_attack = random.randint(1, dragon_max_dmg)
-    # here you need to update the hero hp, you need to subtract the damage that the dragon did
-    # add code on this line
     hero_hp = hero_hp - dragon_attack
 
     print("The dragon does", dragon_attack, "damage and the hero has", hero_hp, "hp left")
-    # add an if condition to check if the hero was killed by the dragon
-    # here goes the if
+
     if hero_hp <= 0:
-        print("Unfortunately the dragon killed our hero. RIP sir Bravealot")
+        print("\nUnfortunately the dragon killed our hero. RIP sir Bravealot")
         break
 
     hero_attack = random.randint(1, hero_max_dmg)
-    # here you need to update the dragon hp, you need to subtract the damage that the hero did
-    # add code on this line
     dragon_hp = dragon_hp - hero_attack
 
-    print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left")
-    # add an if condition to check if the dragon was killed by the hero
-    # here goes the if
+    print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left\n")
+
     if dragon_hp <= 0:
-        print("Our valiant hero has slain the dragon!")
+        print("\nOur valiant hero has slain the dragon!")
         break
 
     input("Round over. Press any key")
